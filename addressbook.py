6f7d7c1416ebfc5d90cd860663c86331ac1bbc3e
hkmtdvlt <hikmetdevlet@gmail.com>
2020-06-14 20:13:05 +0200

    Basic Adress Book
    This is an basic adress book with using dictionary and functions

---
+++ addressbook.py
@@ -0,0 +1,24 @@
+#Project : Basic Address Book 
+#Version 0.01
+#hikmetdevlet@gmail.com
+
+
+adressbook=dict() # Empty Dictionary
+
+
+
+def add_name(name,number): # Function to add names and numbers
+	adressbook[name]=number
+
+print("Press 'q' to quit") 
+
+while True: # Loop to keep program alive until write 'q' 
+	name=input("Write Contact Name: \n") 
+	if name =='q': #Control if user text 'q' or not
+		break # stop the loop
+	number=input("Write the Contact Number: \n")
+	add_name(name,number)#add names and numbers to the dict trough function
+	
+
+
+print(adressbook)#Show all names and numbers
